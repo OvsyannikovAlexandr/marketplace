4ff1a550c74605ecee3397ee28463092f3f6edd4
OvsyannikovAleksandr <ovsyannikov.alv98@gmail.com>
2025-07-04 14:09:24 +0300

    init migration-service

---
+++ migration-service/internal/migrate/runner.go
@@ -0,0 +1,34 @@
+package migrate
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func Run() error {
+	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	m, err := migrate.New(
+		"file://migrations",
+		dbURL,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
+}
